29e72237eb59f39b7058477b30b1fd6bddf309fa
md-786910 <mdashifreza111@gmail.com>
2026-05-11 21:44:15 +0530

    Add api to site!

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,5 @@
 // const API_ENDPOINT = "http://localhost:5000";
-const API_ENDPOINT = "https://leanuptime.duckdns.org";
+// const API_ENDPOINT = "https://leanuptime.duckdns.org";
+const API_ENDPOINT = "https://app.sitelyze.io";
 
 export { API_ENDPOINT };
